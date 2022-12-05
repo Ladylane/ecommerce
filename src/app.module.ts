a6e80618088907aa0c44f181dd4fc98e8044428c
danilo <danilodesousa@gmail.com>
2022-12-05 20:27:53 -0300

    Creating module with orm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,25 @@
-import { PurchaseModule } from './purchase/purchase.module';
 import { PurchaseService } from './purchase/purchase.service';
 import { PurchaseController } from './purchase/purchase.controller';
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
-    
-    
+    TypeOrmModule.forRootAsync({
+      useFactory: async () => ({
+        type: "postgres",
+        host: "localhost",
+        username: "user",
+        password: "user123",
+        database: "ecommerce",
+        synchronize: false,
+        logging: false,
+        entities: [
+          
+        ]
+      })
+    })
+
   ],
   controllers: [
     PurchaseController
